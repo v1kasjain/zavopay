14457a8c64f860fd0a761fe869acc7c9291d0ad6
v1kasjain <vikas.jain@mockingjay.io>
2023-12-11 19:50:23 +0530

    Embed Inter font in head

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className={`${inter.variable} font-sans`}>{children}</body>
     </html>
   );
